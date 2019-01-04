4a1f37bb9ca51df5d4a75f667a6c95a038c0a3c4
Pavel <pavltolmachev@yandex.ru>
2019-01-04 23:17:55 +0300

    Create react website

---
+++ precache-manifest.fa83cbfad9bb2bf28c30d22eb8e57389.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "aebb4cec2ffef54fab95",
+    "url": "/static/js/main.aebb4cec.chunk.js"
+  },
+  {
+    "revision": "3912e494b4cc3a5d3eda",
+    "url": "/static/js/1.3912e494.chunk.js"
+  },
+  {
+    "revision": "aebb4cec2ffef54fab95",
+    "url": "/static/css/main.8250e179.chunk.css"
+  },
+  {
+    "revision": "eed59f3a5608b8fde429809ba5ee925b",
+    "url": "/index.html"
+  }
+];
